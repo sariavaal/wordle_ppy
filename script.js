149a35c511e2928dac8803af0da4b0099cb8db3a
Sara Armoa <62270618+sariavaal@users.noreply.github.com>
2023-12-12 08:25:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('btn').addEventListener('click', () => {
+    const input = document.getElementById('adivinar').value;
+    const valor = input.value
+    });
+    
+    
+})
